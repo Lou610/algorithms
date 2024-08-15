758be3d0ea3ccaa8e9dd179e8b08198070e668ae
TheMeeper <lou@dvbytsolutions.com>
2024-08-15 15:22:37 +0200

    Initial Project

---
+++ Algorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿using Algorithms.Methods;
+
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        //TODO: - Implement all algorithms here
+        static void Main(string[] args)
+        {
+            int answer = BinarySearch.Search(new int[] { 1, 2, 3, 4, 5 }, 3);
+            Console.WriteLine("Your answer is: " + answer);
+        }
+    }
+}
